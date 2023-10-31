5b55a07b0dc7f4d3101ccd069055f4a0dbdba133
GutMaria <gootmaria@gmail.com>
2023-10-31 18:43:43 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,24 @@
-
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 // Спочатку підгружаємо час для відтворення з localStorage
-loadCurrentTime();
-player.on('timeupdate', throttle(saveCurrentTime, 1000) );
-    
+let currentTime = localStorage.getItem('videoplayer-current-time');
+console.log('Збережений час з localStorage: ', currentTime);
+
+if (!currentTime) {
+  console.log('Починаємо спочатку');
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(currentTime);
+}
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
 function saveCurrentTime(evt) {
-
-    const seconds = evt.seconds;
-    localStorage.setItem('videoplayer-current-time', seconds);
-    console.log(evt.seconds);
-    
+  const seconds = evt.seconds;
+  localStorage.setItem('videoplayer-current-time', seconds);
+  console.log(evt.seconds);
 }
-
-
-function loadCurrentTime() {
-    try {
-        const secondForLoad = localStorage.getItem('videoplayer-current-time');
-        player.setCurrentTime(secondForLoad);
-        console.log(secondForLoad);
-        } catch (error) {
-            player.setCurrentTime(0);
-    }
-}
-
-
